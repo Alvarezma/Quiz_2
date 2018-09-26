1b1dc3304e98c17bc234ca6d59f581d2336a9f35
Alvarezma <Derpachu@comcast.net>
2018-09-26 11:02:05 -0600

    Update

---
+++ ThriceSim.java
@@ -0,0 +1,67 @@
+public class ThriceSim {
+
+    public static void main ( String[] args ) {
+        
+        long total = 0;
+        double tempAver;
+        double average = 0.0;
+
+        for (int plays = 1; plays <= 1000; plays++)   {
+
+            long tally = 0;
+            long value = 0;
+            int tripWorth = 0;
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            for (int count = 1; !(a == b && b == c); count++)    {
+                a = (int)(Math.random()*6 + 1);
+                b = (int)(Math.random()*6 + 1);
+                c = (int)(Math.random()*6 + 1);
+                value = a + b + c;
+                if (count % 3 == 0) {
+                    tally = tally - value;
+                } else {
+                    tally = tally + value;
+                }
+                if (a == b && b == c)   {
+                    tripWorth = a;
+                }
+            }
+
+            for (int count = 1; count <= tripWorth; count++)    {
+                a = (int)(Math.random()*6 + 1);
+                b = (int)(Math.random()*6 + 1);
+                c = (int)(Math.random()*6 + 1);
+                value = a * b * c;
+                tally = tally + value;
+            }
+
+            
+            value = 1;
+            while (!(a == b && b == c))  {
+                a = (int)(Math.random()*6 + 1);
+                b = (int)(Math.random()*6 + 1);
+                c = (int)(Math.random()*6 + 1);
+                tally = tally + value;
+                if (a == b && b == c && a == tripWorth)   {
+                    tally = tally * 3;
+                } else if (a == b && b == c)    {
+                    tally = a;
+                }
+                value = value * 3;
+            }
+
+            total = total + tally;
+            if (plays % 100 == 0)   {
+                tempAver = total / 100.0;
+                average = average + tempAver;
+                total = 0;
+            }
+        }
+
+        average = average / 10.0;
+        System.out.println("Average over 1000 games: " + average);
+    }
+}
--- ThriceTurn.java
+++ ThriceTurn.java
@@ -7,29 +7,28 @@ public class ThriceTurn {
         Scanner scan = new Scanner( System.in );
 
         int retire;
-        int tally = 0;
-        int value = 0;
+        long tally = 0;
+        long value = 0;
         int tripWorth = 0;
         int a = 1;
         int b = 2;
         int c = 3;
 
-        for (int count = 1; a != b && b != c; count++)    {
+        for (int count = 1; !(a == b && b == c); count++)    {
             a = (int)(Math.random()*6 + 1);
             b = (int)(Math.random()*6 + 1);
             c = (int)(Math.random()*6 + 1);
-            System.out.println(a);
-            System.out.println(b);
-            System.out.println(c);
+            System.out.println(a + " " + b + " " + c);
             value = a + b + c;
             if (count % 3 == 0) {
                 tally = tally - value;
             } else {
                 tally = tally + value;
             }
+            if (a == b && b == c)   {
+                tripWorth = a;
+            }
         }
-
-        tripWorth = a;
 
         System.out.println("Tally: " + tally);
         System.out.println("Triplet's worth: " + tripWorth);
@@ -38,27 +37,33 @@ public class ThriceTurn {
             a = (int)(Math.random()*6 + 1);
             b = (int)(Math.random()*6 + 1);
             c = (int)(Math.random()*6 + 1);
+            System.out.println(a + " " + b + " " + c);
             value = a * b * c;
             tally = tally + value;
+            System.out.println(value);
+            System.out.println(tally);
         }
 
         System.out.println("Tally: " + tally);
         System.out.println("Do you want to retire?");
         System.out.println("type 0 for yes and 1 for no");
         retire = scan.nextInt();
-
+        
         if (retire == 1)    {
             value = 1;
-            while (a != b && b != c)  {
+            while (!(a == b && b == c))  {
                 a = (int)(Math.random()*6 + 1);
                 b = (int)(Math.random()*6 + 1);
                 c = (int)(Math.random()*6 + 1);
+                System.out.println(a + " " + b + " " + c);
                 tally = tally + value;
                 if (a == b && b == c && a == tripWorth)   {
                     tally = tally * 3;
                 } else if (a == b && b == c)    {
                     tally = a;
                 }
+                System.out.println(value);
+                System.out.println(tally);
                 value = value * 3;
             }
         }
